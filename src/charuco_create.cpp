b9d3b8e98eb53aaf2120d368471d94e08a0d3a64
Johannes Hechtl <johannes.hechtl@tum.de>
2021-01-17 10:16:39 +0100

    added executable for creating charuco board

---
+++ src/charuco_create.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/aruco/charuco.hpp>
+#include <opencv2/highgui.hpp>
+#include <string>
+#include <iostream>
+
+
+int main(int argc, char** argv){
+    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    cv::Ptr<cv::aruco::CharucoBoard> board = cv::aruco::CharucoBoard::create(5, 7, 0.04f, 0.02f, dictionary);
+    cv::Mat boardImage;
+    board->draw(cv::Size(600, 500), boardImage, 10, 1);
+    cv::imwrite("BoardImage.jpg", boardImage);
+}
